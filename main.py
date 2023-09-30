df843243f1dccb048cd4c54ce254a6134a6a3dd9
Timofey228822 <shlatim@yandex.ru>
2023-09-30 18:52:18 +0300

    1 case is right

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-class Solution(object):
-    def romanToInt(self, s):
-        Roman_nums = {
-            'I' : 1,
-            'V' : 5,
-            'X' : 10,
-            'L' : 50,
-            'C' : 100,
-            'D' : 500,
-            'M' : 1000
-            }
-        num = ''
-        r2 = ['I','V','X','L','C','D','M']
 
-        for i in range(0, len(list(s)) - 1):
-            if Roman_nums[r2[i]] > Roman_nums[r2[i + 1]] or Roman_nums[r2[i]] == Roman_nums[r2[i + 1]]:
-                num = num + str(Roman_nums[r2[i]] + Roman_nums[r2[i+1]])
+def romanToInt(s):
+    Roman_nums = {
+        'I' : 1,
+        'V' : 5,
+        'X' : 10,
+        'L' : 50,
+        'C' : 100,
+        'D' : 500,
+        'M' : 1000
+        }
+    num = 0
+    y = 0
+    while y < len(s):
+        num_1 = Roman_nums[s[y]]
+        if y + 1 < len(s):
+            num_2 = Roman_nums[s[y + 1]]
+            if num_1 == num_2 or num_1 > num_2:
+                num += num_1
+        else:
+            if len(s) > 1:
+                previous_num = Roman_nums[s[y - 2]]
+                if num_1 == previous_num:
+                    num += num_1
             else:
-                num = num + str(Roman_nums[r2[i + 1]] - Roman_nums[r2[i]])
-        return int(num)
+                num += num_1
+        y = y + 1
+    return num
+
+print(romanToInt('IV'))
