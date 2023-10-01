adc3edf5b3e0a71fa65b46b5f7b1d1c275f6b8bc
Timofey228822 <shlatim@yandex.ru>
2023-10-01 09:45:53 +0300

    sucesfull

--- main.py
+++ main.py
@@ -17,14 +17,18 @@ def romanToInt(s):
             num_2 = Roman_nums[s[y + 1]]
             if num_1 == num_2 or num_1 > num_2:
                 num += num_1
+                y += 1
+                continue
+            if num_1 < num_2:
+                num += num_2 - num_1
+                y += 2
+                continue
         else:
-            if len(s) > 1:
-                previous_num = Roman_nums[s[y - 2]]
-                if num_1 == previous_num:
+            if len(s) > y:
+                previous_num = Roman_nums[s[y - 1]]
+                if num_1 == previous_num or num_1 < previous_num:
                     num += num_1
-            else:
-                num += num_1
         y = y + 1
     return num
 
-print(romanToInt('IV'))
+print(romanToInt("DCXXI"))
